1bd16b5f15ca0b9751579330252316d2ff17d822
dundumwezi <dunduvibes@gmail.com>
2020-04-25 16:51:44 +0100

    changes

--- dentist/urls.py
+++ dentist/urls.py
@@ -3,9 +3,10 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from dentist import settings
 
-url(r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root':settings.STATIC_ROOT}),
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dento.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+
+#url(r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root':settings.STATIC_ROOT}),
