0772afa5c26150682ca4d574faf76b5d72fbf5d1
dundumwezi <dunduvibes@gmail.com>
2020-04-25 22:53:49 +0100

    tweaked settings

--- dentist/urls.py
+++ dentist/urls.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.views.static import serve
-from dentist import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dento.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+]
