b3ad257745c3a653cc6a4b039328e15ee90fd952
dundumwezi <dunduvibes@gmail.com>
2020-04-25 16:56:29 +0100

    changes

--- dentist/urls.py
+++ dentist/urls.py
@@ -8,5 +8,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dento.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
-
-#url(r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root':settings.STATIC_ROOT}),
