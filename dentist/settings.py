e9fb1bc298e0bf74dc05f37db5c3fcdef6196b59
dundumwezi <dunduvibes@gmail.com>
2020-04-25 22:50:28 +0100

    tweaked settings

--- dentist/settings.py
+++ dentist/settings.py
@@ -27,19 +27,20 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'dento',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'dentist.urls'
@@ -109,11 +110,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = [
-    os.path.join(BASE_DIR, 'staticfiles'),
-]
+STATIC_URL = '/static/'
 
-STATIC_URL = '/staticfiles/'
+STATIC_ROOT = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
