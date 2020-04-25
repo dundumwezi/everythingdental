70061c3251f45deea0f086c9e5c381d3ed7bbaf4
dundumwezi <dunduvibes@gmail.com>
2020-04-25 16:14:15 +0100

    changes

--- dentist/settings.py
+++ dentist/settings.py
@@ -109,6 +109,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATIC_ROOT = [
+    os.path.join(BASE_DIR, 'staticfiles'),
+]
 
 STATIC_URL = '/static/'
 
@@ -126,7 +129,6 @@ EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
 EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #EMAIL_USE_SSL = False
-
 
 django_heroku.settings(locals())
 
--- dentist/urls.py
+++ dentist/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from project_name import settings
+
+url(r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root':settings.STATIC_ROOT}),
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dento.urls')),
